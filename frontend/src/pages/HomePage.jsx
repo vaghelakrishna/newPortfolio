efadaa0d494521b66e335801f8d0bab590a53019
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-06-04 16:53:35 +0530

    add

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import HeroSection from '../components/MakeSenseLogo'
+import AboutMe from '../components/AboutMe'
+import CustomCursor from '../components/CustomCursor'
+import HeresHowSection from '../components/HeresHowSection'
+import Me from '../components/Me'
+import CommunityGarden from '../components/CommunityGarden'
+import MacWorkWindow from '../components/MacWorkWindow'
+import WorkSection from '../section/WorkSection'
+import MetricsSection from '../section/MetricsSection';
+import LogoMarquee from '../components/LogoMarquee'
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <CommunityGarden />
+      <AboutMe />
+
+      <Me />
+      <MetricsSection />
+      <WorkSection />
+      {/* <LogoMarquee /> */}
+      {/* <FilmStrip /> */}
+      <CustomCursor />
+      <HeresHowSection />
+    </>
+  )
+}
