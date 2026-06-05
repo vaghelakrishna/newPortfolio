8808f8f9193b1f099f3d202560ed9313408f9d45
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-06-05 20:09:04 +0530

    commit

---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookFlip from '../components/BookFlip'
+import Navbar from '../components/Navbar'
+
+export default function AboutPage() {
+  return (
+    <>
+      <Navbar />
+    <BookFlip />
+    </>
+  )
+}
