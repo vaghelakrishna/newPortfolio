da65c7d0f7cfd2c2d7d4f5c0b5d8a76de82d35c0
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-06-06 13:14:15 +0530

    add

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BookFlip from '../components/BookFlip'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Cat from '../components/Cat'
 
 export default function AboutPage() {
   return (
     <>
       <Navbar />
-    <BookFlip />
+      <BookFlip />
+      <Cat />
+      <Footer />
     </>
   )
 }
